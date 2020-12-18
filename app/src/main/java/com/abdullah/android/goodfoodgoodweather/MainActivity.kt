17bfa1ad3e31a7e0fa9430ebb199ebd409794178
Abdullahassem <71944810+Abdullahassem@users.noreply.github.com>
2020-12-18 10:38:32 +0300

    added adapter
    now the app retrieves a list of 20 restaurants with thier information

--- app/src/main/java/com/abdullah/android/goodfoodgoodweather/MainActivity.kt
+++ app/src/main/java/com/abdullah/android/goodfoodgoodweather/MainActivity.kt
@@ -2,15 +2,59 @@ package com.abdullah.android.goodfoodgoodweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 private const val TAG="MainActivity"
 private const val BASE_URL= "https://api.yelp.com/v3/"
 private const val API_KEY="C269WVIARBTOJVG8q8hOs7sUj3Awf94kHQZiPbqBwm1mQU3w3dkwLKYROPzSFPb8HNp71C7MIKxlKqY3cJAKnPtxBu2Vb-dzqmDaWnIo8ZgoscelaDY1RUzboN7ZX3Yx"
 private const val WEATHER_API_KEY="615b2c0757484776824113744201612"
 
+
+
+
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val restaurants= mutableListOf<YelpRestaurant>()
+        val adapter=RestaurantsAdapter(this,restaurants)
+        rvRestaurants.adapter=adapter
+        rvRestaurants.layoutManager=LinearLayoutManager(this)
+
+
+
+        val retrofit= Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+
+        val yelpApi= retrofit.create(YelpApi::class.java)
+        yelpApi.searchRestaurants("Bearer $API_KEY","food","Amsterdam").enqueue(object :Callback<YelpSearchResult>{
+
+            override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
+                Log.i(TAG,"onResponse $response")
+                val body=response.body()
+                if(body==null){
+                    Log.w(TAG,"did not recieve body")
+                    return
+                }
+                restaurants.addAll(body.restaurants)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                Log.i(TAG,"onFailure $t")
+            }
+
+        })
     }
+
 }
---
+++ app/src/main/java/com/abdullah/android/goodfoodgoodweather/RestaurantsAdapter.kt
@@ -0,0 +1,46 @@
+package com.abdullah.android.goodfoodgoodweather
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.item_restaurant.view.*
+
+class RestaurantsAdapter(val context: Context,val restaurants: List<YelpRestaurant>) :
+    RecyclerView.Adapter<RestaurantsAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_restaurant,parent,false))
+
+    }
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        fun bind(restaurant: YelpRestaurant) {
+            itemView.name.text=restaurant.name
+            itemView.ratingBar.rating=restaurant.rating.toFloat()
+            itemView.numReviews.text="${restaurant.numReviews} Reviews"
+            itemView.address.text=restaurant.location.address
+            itemView.category.text=restaurant.categories[0].title
+            itemView.distance.text=restaurant.displayDistance()
+            itemView.price.text=restaurant.price
+
+
+        }
+    }
+
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val restaurant=restaurants[position]
+        holder.bind(restaurant)
+    }
+
+    override fun getItemCount()=restaurants.size
+
+}
