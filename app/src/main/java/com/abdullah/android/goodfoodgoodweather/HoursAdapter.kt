879fc6824aeb57389756b31755a515d4a0fb79d2
Abdullahassem <71944810+Abdullahassem@users.noreply.github.com>
2020-12-25 15:25:29 +0300

    changed to a grid look

--- app/src/main/java/com/abdullah/android/goodfoodgoodweather/HoursAdapter.kt
+++ app/src/main/java/com/abdullah/android/goodfoodgoodweather/HoursAdapter.kt
@@ -1,19 +1,38 @@
 package com.abdullah.android.goodfoodgoodweather
 
+<<<<<<< Updated upstream
+=======
+import android.util.Log
+>>>>>>> Stashed changes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.abdullah.android.goodfoodgoodweather.Models.Hour
 import com.abdullah.android.goodfoodgoodweather.databinding.HoursWeatherInfoBinding
+<<<<<<< Updated upstream
+=======
+import com.bumptech.glide.Glide
+>>>>>>> Stashed changes
 
 class HoursAdapter(private val hoursList: ArrayList<Hour>) : RecyclerView.Adapter<HoursAdapter.HoursViewHolder>() {
 
     inner class HoursViewHolder(val binding: HoursWeatherInfoBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(hour: Hour) {
+<<<<<<< Updated upstream
             binding.temp.text = hour.temp_c.toString()
             binding.time.text=hour.time
+=======
+
+            binding.temp.text = "${hour.temp_c.toString()}°"
+            binding.time.text=hour.time.substring(11)
+
+
+            // glide(itemview), https, into the image
+
+
+>>>>>>> Stashed changes
         }
     }
 
