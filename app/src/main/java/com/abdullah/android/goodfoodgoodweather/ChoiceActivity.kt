5c0332cf3bcabe835850351db7cebefb800ab5ce
Abdullahassem <71944810+Abdullahassem@users.noreply.github.com>
2020-12-30 14:54:50 +0300

    added a search bar for the user to lookup whatever he wants

--- app/src/main/java/com/abdullah/android/goodfoodgoodweather/ChoiceActivity.kt
+++ app/src/main/java/com/abdullah/android/goodfoodgoodweather/ChoiceActivity.kt
@@ -13,6 +13,8 @@ private lateinit var pizzaImageView:ImageView
 private lateinit var chickenImageView:ImageView
 private lateinit var cakeImageView:ImageView
 private lateinit var iceCreamImageView:ImageView
+private lateinit var editText: EditText
+private lateinit var button: Button
 
 class ChoiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +27,8 @@ class ChoiceActivity : AppCompatActivity() {
         chickenImageView=findViewById(R.id.chicken)
         cakeImageView=findViewById(R.id.cake)
         iceCreamImageView=findViewById(R.id.iceCream)
+        editText=findViewById(R.id.editText)
+        button=findViewById(R.id.button)
 
         val lon=intent.getDoubleExtra("longitude",0.0)
         val lat=intent.getDoubleExtra("latitude",0.0)
@@ -53,6 +57,10 @@ class ChoiceActivity : AppCompatActivity() {
         }
         iceCreamImageView.setOnClickListener {
             passTo("Ice Cream")
+        }
+
+        button.setOnClickListener {
+            passTo(editText.text.toString())
         }
 
 
